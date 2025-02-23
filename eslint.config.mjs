381348153b20732541e681f2cd062a41bead5571
Nelio Bila <neliobilla@gmail.com>
2025-02-23 21:34:50 +0200

    fix; auth working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off", // Disable the rule entirely
+      "@typescript-eslint/no-empty-object-type": "off"
     },
   },
 ];
--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,9 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
-import Credentials from "next-auth/providers/credentials";
 import { SafeUserType } from "@/types";
-import { DefaultSession } from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
+import Credentials from "next-auth/providers/credentials";
 import { login } from "./actions/auth-actions";
+import { authConfig } from "./auth.config";
 export const {
   auth,
   signIn,
@@ -26,7 +25,7 @@ export const {
   ],
   session: {
     strategy: "jwt",
-    maxAge: 15 * 60, // 15 minutes in seconds (matching Django's ACCESS_TOKEN_LIFETIME)
+    // maxAge: 15 * 60, // 15 minutes in seconds (matching Django's ACCESS_TOKEN_LIFETIME)
   },
   callbacks: {
     async session({ session, token }) {
